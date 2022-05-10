e52c363fc34581543c3d91a191889215ce40ddf2
Marlon Velasquez <marlonjvelasquez@gmail.com>
2022-05-10 22:19:47 +0200

    fix path of sw.js

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,47 +1,47 @@
+let cacheName = "V.0.9.5";
+let assets = [
+  "index.html",
+  "./scripts/script.js",
+  "./scripts/sw.js",
+  "./scripts/changeBackground",
+  "./icon/icon-192x192.png",
+];
 
-    let cacheName = "V.0.9.5"
-    let assets = [
-    "index.html",
-    "./scripts/script.js",
-    "./scripts/sw.js",
-    "./scripts/changeBackground",
-    "./icon/icon-192x192.png"
-    ]
-
-    self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-        caches.open(cacheName).then(cache => {
-        cache.addAll(assets)
-        })
-    )
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      cache.addAll(assets);
     })
+  );
+});
 
-    self.addEventListener("fetch", fetchEvent => {
-        fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
-        })
-        )
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
     })
+  );
+});
 
-    self.addEventListener('activate', cleanEvent => {
-    cleanEvent.waitUntil(
-        caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-            if(key !== assets) {
+self.addEventListener("activate", (cleanEvent) => {
+  cleanEvent.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== assets) {
             return caches.delete(key);
-            }
-        }));
+          }
         })
-    );
-    });
-
-    if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-        navigator.serviceWorker
-        .register("./sw.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
+      );
     })
-    }          
-    
+  );
+});
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("./src/sw.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
