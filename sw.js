30388b0315ae49625830e0b872a4d5b9fa24a58c
sargentogato <53468548+sargentogato@users.noreply.github.com>
2022-05-13 02:29:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 let cacheName = "V.0.9.5";
 let assets = [
   "index.html",
-  "./src/scripts/script.js",
+  "/src/scripts/script.js",
   "/sw.js",
-  "./src/scripts/changeBackground.js",
-  "./src/icons/icon-192x192.png",
+  "/src/scripts/changeBackground.js",
+  "/src/icons/icon-192x192.png",
 ];
 
 self.addEventListener("install", (installEvent) => {
