32e09bf5a44fda06cc324cb8d1db37966efe106b
Marlon Velasquez <marlonjvelasquez@gmail.com>
2022-07-02 12:40:59 +0530

    change path main icon

--- sw.js
+++ sw.js
@@ -1,27 +1,28 @@
-let cacheName = "V.0.9.5";
+let cacheName = "V.0.9.5"
 let assets = [
+  // prettier-ignore
   "index.html",
   "./src/scripts/script.js",
   "./sw.js",
   "./src/scripts/changeBackground.js",
-  "./src/icons/icon-192x192.png",
-];
+  ".src/icons/android/android-launchericon-192-192.png",
+]
 
 self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(
     caches.open(cacheName).then((cache) => {
-      cache.addAll(assets);
+      cache.addAll(assets)
     })
-  );
-});
+  )
+})
 
 self.addEventListener("fetch", (fetchEvent) => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then((res) => {
-      return res || fetch(fetchEvent.request);
+      return res || fetch(fetchEvent.request)
     })
-  );
-});
+  )
+})
 
 self.addEventListener("activate", (cleanEvent) => {
   cleanEvent.waitUntil(
@@ -29,19 +30,19 @@ self.addEventListener("activate", (cleanEvent) => {
       return Promise.all(
         keyList.map((key) => {
           if (key !== assets) {
-            return caches.delete(key);
+            return caches.delete(key)
           }
         })
-      );
+      )
     })
-  );
-});
+  )
+})
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
       .register("sw.js")
       .then((res) => console.log("service worker registered"))
-      .catch((err) => console.log("service worker not registered", err));
-  });
+      .catch((err) => console.log("service worker not registered", err))
+  })
 }
